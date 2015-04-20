dfd7ec66efbdb4d75a3323d7ba38a55ddcc580b6
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-04-20 12:03:18 +0200

    remove explicit user

--- trello_workon_upstart.sh
+++ trello_workon_upstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /srv/sites/trello_workon/env.sh
 cd /srv/sites/trello_workon/trello_workon
-exec /srv/sites/trello_workon/trello_workon/.env/bin/gunicorn -u deploy -c gunicorn_config.py management.wsgi
+exec /srv/sites/trello_workon/trello_workon/.env/bin/gunicorn -c gunicorn_config.py management.wsgi
