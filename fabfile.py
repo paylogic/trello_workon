0dfe76774cadb50af15ba30d18f8c93232e4a039
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-04-20 11:44:02 +0200

    update ip

--- fabfile.py
+++ fabfile.py
@@ -3,21 +3,18 @@ Fabric deployment script
 
 """
 
-from fabric.api import task, run
+from fabric.api import task, run, cd
 
-env.hosts = ['10.0.30.52']
+env.hosts = ['10.0.31.52']
 
 
 @task
 def deploy():
     """Deploy the current version on github to the server."""
 
-    # TODO: stop server
-
     cd('/srv/sites/trello_workon/trello_workon')
     run('git pull')
     run('virtualenv .env')
     run('. .env/bin/activate')
     run('pip install -r requirements.txt')
-
-    # TODO: start server
+    cd('sudo service trello_workon restart')
--- trello_workon_upstart.sh
+++ trello_workon_upstart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 . /srv/sites/trello_workon/env.sh
 cd /srv/sites/trello_workon/trello_workon
-exec /srv/sites/trello_workon/trello_workon/.env/bin/gunicorn -u www-data -c gunicorn_config.py management.wsgi
-
+exec /srv/sites/trello_workon/trello_workon/.env/bin/gunicorn -u deploy -c gunicorn_config.py management.wsgi
